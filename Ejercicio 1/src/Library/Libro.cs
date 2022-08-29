9334deb571303f8f8578d68dfc8e8328b6dc690d
ManuelM512 <110827049+ManuelM512@users.noreply.github.com>
2022-08-29 10:27:04 -0300

    Update Libro.cs

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -8,21 +8,27 @@ namespace SRP
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
-        public string LibrarySector { get ; set; }
+        //No estaría bien que una clase Libro contenga su posición en la biblioteca, sino delegar la responsabilidad de las posiciones a otras clases
+        public string LibrarySector { get ; set; } 
         public string LibraryShelve { get ; set; }
-
+        //Sería más coherente que se formulase una clase de sectores y otra de estanterías para poder guardar cada uno a la otra de manera ordenada, y a los libros
         public Book(String title, String author, String code)
         {
             this.Title = title;
             this.Author = author;
             this.Code = code;
         }
-
+        
         public void ShelveBook(String sector, String shelve)
         {
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
 
+    }
+    
+    public class Shelve{
+        public ArrayList books = new ArrayList();
+        
     }
 }
